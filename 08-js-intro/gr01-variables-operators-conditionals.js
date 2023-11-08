cc5991f6c06b7bf009f0a8ec9cfbf2da3cdd8357
lucas! <carralas@icloud.com>
2023-11-08 18:03:20 -0300

    style: change variable names according to convention

--- 08-js-intro/gr01-variables-operators-conditionals.js
+++ 08-js-intro/gr01-variables-operators-conditionals.js
@@ -1,14 +1,14 @@
-const preco_gasolina_l = 5.65; /* preço no dia 08/11/2023 */
-const preco_etanol_l = 3.64; /* preço no dia 08/11/2023 */
-const tipo_combustivel_usado = 'gasolina';
-const custo_km_por_l = 10.2; /* referente ao carro kia soul */
-const distancia_km = 12.7; /* comprimento da ponte rio-niterói */
-var gasto_total;
+const precoGasolinaLitro = 5.65; /* preço no dia 08/11/2023 */
+const precoEtanolLitro = 3.64; /* preço no dia 08/11/2023 */
+const custoKmLitro = 10.2; /* referente ao carro kia soul */
+const distanciaKm = 12.7; /* comprimento da ponte rio-niterói */
+const tipoCombustivelUsado = 'gasolina';
+var gastoTotal;
 
-if (tipo_combustivel_usado === 'gasolina') {
-    gasto_total = (distancia_km/custo_km_por_l)*preco_gasolina_l;
+if (tipoCombustivelUsado === 'gasolina') {
+    gastoTotal = (distanciaKm/custoKmLitro)*precoGasolinaLitro;
 } else {
-    gasto_total = (distancia_km/custo_km_por_l)*preco_etanol_l;
+    gastoTotal = (distanciaKm/custoKmLitro)*precoEtanolLitro;
 }
 
-console.log(`combustivel: ${tipo_combustivel_usado}. gasto total: ${gasto_total.toFixed(2)}.`)
+console.log(`combustivel: ${tipoCombustivelUsado}. gasto total: ${gastoTotal.toFixed(2)}.`)
